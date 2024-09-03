3fbb568066d2f25defc517b74d3bf5835cbd7c21
Nazmulshuvo123 <nazmulshuvo161199@gmail.com>
2024-09-03 12:54:27 +0600

    concat two array

---
+++ task-5.js
@@ -0,0 +1,26 @@
+/**
+ * Combining Arrays
+Instructions:
+
+Create two arrays of your choice.
+Use the concat method to combine the two arrays into a new array.
+Print both the original arrays and the combined array using console.log().
+ */
+
+// Creating two array
+const cars = ["Roles Royels", "BMW", "Ferrari"];
+const bikes = ["Royel Enfield", "Jawa", "Sujuki"];
+
+//Concat the two array
+let myCollection = cars.concat(bikes);
+
+console.log("The orginal array is: ", cars);
+
+console.log("The orginal array is: ", bikes);
+
+//Concat array
+
+
+console.log("The concat array is: ", myCollection);
+
+
