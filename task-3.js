bb3205da1ff7f634b58cd41f636764e15eab824d
Nazmulshuvo123 <nazmulshuvo161199@gmail.com>
2024-09-03 12:22:07 +0600

    useing include method to find item an array

---
+++ task-3.js
@@ -0,0 +1,19 @@
+/*
+3. Checking Array Membership with ‘includes’
+Instructions:
+
+Create an array of books containing different book.
+
+Use the includes method to check if the array contains a javascript book.
+
+Print a message to the console indicating whether the element is present in the array or not.
+
+*/
+
+let books = ["Bangla", "Math", "JS", "English", "chemisty"];
+
+let book = books.includes("JS");
+
+console.log(books);
+
+console.log(book);
